ea15cef1806c3e99f30e9b0841f60a77bcc0f78f
Suvansh <113202968+suvi04@users.noreply.github.com>
2022-09-27 14:46:14 +0530

    Add files via upload

---
+++ bill calc.py
@@ -0,0 +1,9 @@
+amt=input("Enter amount ")
+no=input("Enter quantity ")
+c=int(amt)*int(no)
+print(c)
+while 0<1:
+    amt=input("Enter amount ")
+    no=input("Enter quantity ")
+    c=int(c)+(int(amt)*int(no))
+    print(c)
